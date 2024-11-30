88fdd54e7c7989b8ffbd917d617743d42e63eda7
xuzhong.ubuntu <xu.zhong@hotmail.com>
2024-11-30 21:44:53 +0800

    ok

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
 python main.py
+
+echo Config network proxy on windows
+echo --------------------------------------------------
+echo netsh interface show interface
+echo netsh interface ipv4 set interface "Wi-Fi" forwarding=enable
+echo netsh interface portproxy add v4tov4 listenport=8188 listenaddress=0.0.0.0 connectport=8188 connectaddress=172.31.21.142
+echo --------------------------------------------------
